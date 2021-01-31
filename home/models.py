bd881e9c6765ec6239f2417bd694a2c81780c199
kris965 <kristian_hoogland@hotmail.com>
2021-01-31 18:32:13 +0000

    added item fields to model

--- home/models.py
+++ home/models.py
@@ -1,11 +1,35 @@
 from django.conf import settings
 from django.db import models
 
+CATEGORY_CHOICES = (
+    ('APA', 'American Pale Ale'),
+    ('IPA', 'Indian Pale Ale'),
+    ('S', 'Stout'),
+    ('WB', 'Wheat Beer'),
+    ('PO', 'Porter'),
+    ('BPA', 'Belgian Pale Ale'),
+    ('B', 'Bock'),
+    ('EPA', 'English Pale Ale'),
+    ('P', 'Pilsner'),
+    ('D', 'Dubbel'),
+    ('T', 'Triple'),
+    ('Q', 'Quadruple')
+)
+
+
+LABEL_COLORS = (
+    ('O', 'orange'),
+    ('B', 'brown'),
+    ('Y', 'yellow')
+)
+
 
 # Displays items available for purchase in the store #
 class Item(models.Model):
     title = models.CharField(max_length=100)
-    price = models.FloatField()
+    brewer = models.FloatField()
+    style = models.CharField(choices=CATEGORY_CHOICES, max_length=20)
+    label = models.CharField(choices=LABEL_COLORS, max_length=1)
 
     def __str__(self):
         return self.title
